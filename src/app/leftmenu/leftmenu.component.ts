d0e613f09d12e33e0e9e595ea605f6a1462f4db5
Тимофей Карклин <timkar164@gmail.com>
2021-04-15 21:09:13 +0300

    lastcom

--- src/app/leftmenu/leftmenu.component.ts
+++ src/app/leftmenu/leftmenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-leftmenu',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftmenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  onlodaut(){
+    window.localStorage.clear();
+    this.router.navigate(['/auth']);
+  }
 }
